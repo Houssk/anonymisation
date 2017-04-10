478f12cfcea65881e4c8cdbf38234b4f02d35e52
houssk <karrach.houssam@gmail.com>
2017-04-10 17:24:00 +0200

    modified link

--- Program.cs
+++ Program.cs
@@ -19,17 +19,21 @@ namespace AnoDCM
         {
             // Déclaration et initialisation des valeurs d'entrées 
             string PPN_plateforme = args[0];
-            string PathFich = args[1];
+            string PathBegin = args[1];
+            string zip = args[2];
+            string PathFich = PathBegin + @"\"+ zip;
             int PPN = int.Parse(PPN_plateforme);// Permet de convertir le string en int pour renommer le header du DICOM
             string zipPath = PathFich +".zip" ;
 
             // Création du dossier où on va décompresser les images du dossier .zip
-            System.IO.Directory.CreateDirectory(PathFich + @"\Anonymisation");
-            string extractPath = PathFich + @"\Anonymisation";
-
+            System.IO.Directory.CreateDirectory(PathBegin + @"\anonymisation\anonymisation");
+         
+            string extractPath = PathBegin + @"\anonymisation";
+           
+            string extract = extractPath + @"\anonymisation";
             // Méthode qui permet la décompression du dossier
-            ZipFile.ExtractToDirectory(zipPath, extractPath);
-
+            ZipFile.ExtractToDirectory(zipPath, extract);
+            System.IO.Directory.SetCurrentDirectory(extractPath);
             // Déclartion du booléen qui va valider l'anonymisation
             bool validation = false;
 
@@ -59,13 +63,16 @@ namespace AnoDCM
             connect = Anonymisation.ConnectBDD();
 
             // Méthode qui va lancer l'anonymisation des DICOM avec la fonction Anonymize de la classe Traitement. Cette focntion retourne un booléen pour valider le bon fonctionnement de celle ci.
-            validation=Anonymisation.Anonymize(extractPath, PPN_plateforme);
+            validation=Anonymisation.Anonymize(extract, PPN_plateforme);
 
             // Méthode qui va valider l'anonymisation des DICOM et qui va aller écrire dans la base de données la valeur correspondante (1: A fonctionné 0: N'a pas fonctionné)
             Anonymisation.ValidationAnon(PPN, connect, validation);
    
             // Méthode qui permet de créer un dossier compressé des images DICOM anonymisées
-             ZipFile.CreateFromDirectory(extractPath, PathFich+"Anonymisation.zip");
+             //ZipFile.CreateFromDirectory(@"\anonymisation", zip+".zip");
+           // string ano = extractPath + @"\anonymisation";
+             ZipFile.CreateFromDirectory("anonymisation", zip + ".zip");
+             System.IO.Directory.Delete(extract,true);
             
         }
     
