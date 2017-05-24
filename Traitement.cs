bf70bd6eef76d497f5e068328eca1e059ad8dcea
houssk <karrach.houssam@gmail.com>
2017-05-24 16:37:57 +0200

    nouveaux champs BDD verification

--- Traitement.cs
+++ Traitement.cs
@@ -130,7 +130,7 @@ namespace AnoDCM
 
                 }
                 // return ret;
-                Console.WriteLine(compteurWrite);
+              //  Console.WriteLine(compteurWrite);
             }
 
             val = ret;
