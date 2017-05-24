7688b75d721783534c6a4f294377f8bced19d77f
houssk <karrach.houssam@gmail.com>
2017-05-24 16:36:46 +0200

    nouveaux champs BDD verification

--- Program.cs
+++ Program.cs
@@ -16,21 +16,21 @@ namespace AnoDCM
 {
     class Program
     {
-        public static void Main( string[] args )
+        public static void Main(string[] args)
         {
             // Déclaration et initialisation des valeurs d'entrées 
             string PPN_plateforme = args[0];
             string PathBegin = args[1];
             string zip = args[2];
-            string PathFich = PathBegin + @"\"+ zip;
+            string PathFich = PathBegin + @"\" + zip;
             int PPN = int.Parse(PPN_plateforme);// Permet de convertir le string en int pour renommer le header du DICOM
-            string zipPath = PathFich +".zip" ;
+            string zipPath = PathFich + ".zip";
 
             // Création du dossier où on va décompresser les images du dossier .zip
             System.IO.Directory.CreateDirectory(PathBegin + @"\anonymisation\anonymisation");
-         
+
             string extractPath = PathBegin + @"\anonymisation";
-           
+
             string extract = extractPath + @"\anonymisation";
             // Méthode qui permet la décompression du dossier
             ZipFile.ExtractToDirectory(zipPath, extract);
@@ -56,7 +56,7 @@ namespace AnoDCM
 
             }
 
-            
+
             // Déclaration et Initialisation de la classe traitement
             Traitement Anonymisation = new Traitement();
 
@@ -66,29 +66,29 @@ namespace AnoDCM
 
 
             // Méthode qui va lancer l'anonymisation des DICOM avec la fonction Anonymize de la classe Traitement. Cette focntion retourne un booléen pour valider le bon fonctionnement de celle ci.
-           // validation=Anonymisation.Anonymize(extract, PPN_plateforme,0,500);
+            // validation=Anonymisation.Anonymize(extract, PPN_plateforme,0,500);
 
             validation = Anonymisation.GestionNbFichier(extract, PPN_plateforme);
-            
+
             // Méthode qui va valider l'anonymisation des DICOM et qui va aller écrire dans la base de données la valeur correspondante (1: A fonctionné 0: N'a pas fonctionné)
-           if(zip == "mandibule")
-           {
-               Anonymisation.ValidationAnonM(PPN, connect, validation);
-           }
-            
-            if(zip == "angioscanner")
+            if (zip == "mandibule")
+            {
+                Anonymisation.ValidationAnonM(PPN, connect, validation);
+            }
+
+            if (zip == "angioscanner")
             {
                 Anonymisation.ValidationAnonA(PPN, connect, validation);
             }
-   
+            Anonymisation.verification(zip, connect, PPN);
             // Méthode qui permet de créer un dossier compressé des images DICOM anonymisées
-             //ZipFile.CreateFromDirectory(@"\anonymisation", zip+".zip");
-           // string ano = extractPath + @"\anonymisation";
-             ZipFile.CreateFromDirectory("anonymisation", zip + ".zip");
-             System.IO.Directory.Delete(extract,true);
-            
+            //ZipFile.CreateFromDirectory(@"\anonymisation", zip+".zip");
+            // string ano = extractPath + @"\anonymisation";
+            ZipFile.CreateFromDirectory("anonymisation", zip + ".zip");
+            System.IO.Directory.Delete(extract, true);
+
         }
-    
+
     }
 }
 
--- Traitement.cs
+++ Traitement.cs
@@ -25,174 +25,213 @@ namespace AnoDCM
         }
 
         //-------------------------------------------------------------------------------------------------------
-           public MySqlConnection ConnectBDD()
-         {
-           MySql.Data.MySqlClient.MySqlConnection conn = new MySql.Data.MySqlClient.MySqlConnection();
+        public MySqlConnection ConnectBDD()
+        {
+            MySql.Data.MySqlClient.MySqlConnection conn = new MySql.Data.MySqlClient.MySqlConnection();
 
-               
-             // Méthode qui va initialiser le builder :
+
+            // Méthode qui va initialiser le builder :
              SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
 
 
-             // renseigner les paramètres de la connexion :
-             builder.DataSource = "127.0.0.1";     // adresse du serveur
-             builder.InitialCatalog = "globald";   // nom bdd
-             builder.UserID = "root";              // id de l'utilisateur                                           VERSION SERVEUR
-             builder.Password = "On30rth0M3d!cal";                // mot de passe
+               // renseigner les paramètres de la connexion :
+              builder.DataSource = "127.0.0.1";     // adresse du serveur
+              builder.InitialCatalog = "globald";   // nom bdd
+              builder.UserID = "root";              // id de l'utilisateur                                           VERSION SERVEUR
+              builder.Password = "On30rth0M3d!cal";                // mot de passe
 
-             /*builder.DataSource = "127.0.0.1";     // adresse du serveur
-             builder.InitialCatalog = "globald";   // nom bdd
-             builder.UserID = "root";              // id de l'utilisateur                                           VERSION TEST
-             builder.Password = "";                // mot de passe*/
+              /*builder.DataSource = "127.0.0.1";     // adresse du serveur
+               builder.InitialCatalog = "globald";   // nom bdd
+               builder.UserID = "root";              // id de l'utilisateur                                           VERSION TEST
+               builder.Password = "";                // mot de passe*/
 
-             // enfin passer la ConnectionString à l'objet SqlConnection
-             string connString = builder.ConnectionString;
-             MySqlConnection cn = new MySqlConnection(connString);
-             try
-             {
-                 cn.Open();
-                 Console.WriteLine("Connexion Active!"); // Affiche sur la console que la connexion est active
-                 
-             }
-             catch (MySqlException ex)
-             {
-                 Console.WriteLine("Echec de connexion" + ex.Message); // Affiche sur la console que la connexion n'est pas active
+              // enfin passer la ConnectionString à l'objet SqlConnection
+            string connString = builder.ConnectionString;
+            MySqlConnection cn = new MySqlConnection(connString);
+            try
+            {
+                cn.Open();
+                Console.WriteLine("Connexion Active!"); // Affiche sur la console que la connexion est active
 
-             }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Echec de connexion" + ex.Message); // Affiche sur la console que la connexion n'est pas active
+
+            }
 
 
-             return cn;
-         }
+            return cn;
+        }
 
-           //-------------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------------
+        bool verification1 = false;
+        public void Anonymize(string chemin, string nom, int debut, int fin, string[] filename)
+        {
 
-         public void Anonymize(string chemin, string nom,int debut, int fin, string [] filename)
-         {
-             
-             string PPN = nom;
-             string cheminDoss= chemin;           
-             string patname = "PPN "+PPN;
+            string PPN = nom;
+            string cheminDoss = chemin;
+            string patname = "PPN " + PPN;
 
-             // SearchOption.AllDirectories permet de gérer tous les répertoires et les sous repertoires pour accéder au dossier contenant les images
-             
-             int nbfilename = System.IO.Directory.GetFiles(cheminDoss, "*.*", SearchOption.AllDirectories).Length;
-             
-             Tag pattag = new Tag(0x0010, 0x0010);
-             bool ret = true;
-             bool val = false;
-          /*   int comptReadFileT = 0;
-             int comptReadFileF = 0;
-             int comptReadAnoT = 0;
-              int comptReadAnoF = 0;*/
-            
-             
-             Console.WriteLine(filename.Length);
-  
-             for (int i = debut; i < fin; i++)
-             {
-                 Anonymizer ano = new Anonymizer();
-                 Reader reader = new Reader();
-               
-                 
+            // SearchOption.AllDirectories permet de gérer tous les répertoires et les sous repertoires pour accéder au dossier contenant les images
+
+            int nbfilename = System.IO.Directory.GetFiles(cheminDoss, "*.*", SearchOption.AllDirectories).Length;
+
+            Tag pattag = new Tag(0x0010, 0x0010);
+            bool ret = true;
+            bool val = false;
+
+
+
+            Console.WriteLine(filename.Length + " debut boucle v5");
+
+            for (int i = debut; i < fin; i++)
+            {
+                int compteurWrite = 0;
+                Anonymizer ano = new Anonymizer();
+                Reader reader = new Reader();
+
+
                 // ret = reader.Read();
-                 reader.SetFileName(filename[i]);
-                 reader.Read();
-                 /*  if (reader.Read()) {
-                     comptReadFileT++;
-                 }
-                 else
-                 {
-                     comptReadFileF++;
-                 }*/
+                reader.SetFileName(filename[i]);
+                reader.Read();
+                /*  if (reader.Read()) {
+                    comptReadFileT++;
+                }
+                else
+                {
+                    comptReadFileF++;
+                }*/
 
-                 ano.SetFile(reader.GetFile());
-                 ano.Replace(pattag, patname);
-               /*  if (!ano.Replace(pattag, patname))
-                 {
-                     comptReadAnoT++;
-                 }
-                 else
-                 {
-                     comptReadAnoF++;
-                 }*/
-                 ano.RemovePrivateTags();
+                ano.SetFile(reader.GetFile());
+                ano.Replace(pattag, patname);
+                ano.RemovePrivateTags();
 
 
-                 Writer writer = new Writer();
-                 writer.SetFileName(filename[i]);
-                 writer.SetFile(ano.GetFile());
+                Writer writer = new Writer();
+                writer.SetFileName(filename[i]);
+                writer.SetFile(ano.GetFile());
 
-                 ret = writer.Write();
-                 if (!ret)
-                 {
-                     ret = false;
-                               
-                 }
+                ret = writer.Write();
+                while (!ret)
+                {
+                    // compteurWrite++;
+                    writer = new Writer();
+                    writer.SetFileName(filename[i]);
+                    writer.SetFile(ano.GetFile());
+                    ret = writer.Write();
+                    compteurWrite++;
+                    if (compteurWrite > 100000)
+                    {
+                        ret = true;
+                        verification1 = true;
+                    }
+                    //ret = false;
+
+                }
                 // return ret;
-                 
-             }
-            
-             val = ret;
+                Console.WriteLine(compteurWrite);
+            }
+
+            val = ret;
             // return val;
-         }
+        }
 
-         //-------------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------------
 
-         public void ValidationAnonA(int PPN, MySqlConnection conn, bool valid)
-         {
-             bool ok = valid;
-             //ok = false;
-             int val=0;
+        public void ValidationAnonA(int PPN, MySqlConnection conn, bool valid)
+        {
+            bool ok = valid;
+            //ok = false;
+            int val = 0;
 
-             if (ok == true)
-             {
-                 val = 1;
-             }
-             else val =0;
+            if (ok == true)
+            {
+                val = 1;
+            }
+            else val = 0;
 
-             MySqlConnection connect;
-             connect = conn;
+           MySqlConnection connect;
+            connect = conn;
 
-             MySqlCommand maCommande = new MySqlCommand("UPDATE  operation SET  anonymisationA= @val WHERE ppn_operation=" + PPN, connect); //anonymisationA
-             maCommande.Parameters.Add("@val", MySqlDbType.Int32).Value = val;
-             MySqlDataReader reader;
-             
-             reader = maCommande.ExecuteReader();
+            MySqlCommand maCommande = new MySqlCommand("UPDATE  operation SET  anonymisationA= @val WHERE ppn_operation=" + PPN, connect); //anonymisationA
+            maCommande.Parameters.Add("@val", MySqlDbType.Int32).Value = val;
+            MySqlDataReader reader;
 
-         }
-         public void ValidationAnonM(int PPN, MySqlConnection conn, bool valid)
-         {
-             bool ok = valid;
-             //ok = false;
-             int val = 0;
+            reader = maCommande.ExecuteReader();
+            connect.Close();
 
-             if (ok == true)
-             {
-                 val = 1;
-             }
-             else val = 0;
+        }
+        public void verification(string nomZip, MySqlConnection conn,int PPN)
+        {
+            if (verification1 == true)
+            {
+                if (nomZip == "mandibule")
+                {
 
-             MySqlConnection connect;
-             connect = conn;
+                    int val = 1;
+                    MySqlConnection connect;
+                    connect = conn;
+                    conn.Open();
+                    MySqlCommand maCommande = new MySqlCommand("UPDATE  operation SET  anonymisationOkM= @val WHERE ppn_operation=" + PPN, connect); //anonymisationM
+                    maCommande.Parameters.Add("@val", MySqlDbType.Int32).Value = val;
+                    MySqlDataReader reader;
 
-             MySqlCommand maCommande = new MySqlCommand("UPDATE  operation SET  anonymisationM= @val WHERE ppn_operation=" + PPN, connect); //anonymisationM
-             maCommande.Parameters.Add("@val", MySqlDbType.Int32).Value = val;
-             MySqlDataReader reader;
+                    reader = maCommande.ExecuteReader();
+                }
+                else if (nomZip == "angioscanner")
+                {
+                    int val = 1;
+                    MySqlConnection connect;
+                    connect = conn;
+                    conn.Open();
 
-             reader = maCommande.ExecuteReader();
+                    MySqlCommand maCommande = new MySqlCommand("UPDATE  operation SET  anonymisationOkA= @val WHERE ppn_operation=" + PPN, connect); //anonymisationM
+                    maCommande.Parameters.Add("@val", MySqlDbType.Int32).Value = val;
+                    MySqlDataReader reader;
 
-         }
+                    reader = maCommande.ExecuteReader();
+                }
+             //   connect.Close();
+            }
+            
+        }
+        public void ValidationAnonM(int PPN, MySqlConnection conn, bool valid)
+        {
+            bool ok = valid;
+            //ok = false;
+            int val = 0;
+
+            if (ok == true)
+            {
+                val = 1;
+            }
+            else val = 0;
+
+            MySqlConnection connect;
+            connect = conn;
+
+            MySqlCommand maCommande = new MySqlCommand("UPDATE  operation SET  anonymisationM= @val WHERE ppn_operation=" + PPN, connect); //anonymisationM
+            maCommande.Parameters.Add("@val", MySqlDbType.Int32).Value = val;
+            MySqlDataReader reader;
+
+            reader = maCommande.ExecuteReader();
+            connect.Close();
+
+        }
+
+
 
         // Nouvelle fonction pour la gestion du nombre de fichiers contenus dans un dossier
         public bool GestionNbFichier(string chemin, string nom)
-         {
-           bool val = true;
-           string[] filename = new string[0];
-           filename = System.IO.Directory.GetFiles(chemin, "*.*", SearchOption.AllDirectories);
-           int nbfilename = System.IO.Directory.GetFiles(chemin, "*.*", SearchOption.AllDirectories).Length;
+        {
+            bool val = true;
+            string[] filename = new string[0];
+            filename = System.IO.Directory.GetFiles(chemin, "*.*", SearchOption.AllDirectories);
+            int nbfilename = System.IO.Directory.GetFiles(chemin, "*.*", SearchOption.AllDirectories).Length;
 
-           int nb = nbfilename / 500;
-           int modulo = nbfilename % 500;
+            int nb = nbfilename / 500;
+            int modulo = nbfilename % 500;
             if (nb == 0)
             {
                 nb = 1;
@@ -200,36 +239,36 @@ namespace AnoDCM
 
             else
             {
-                if(modulo!=0)
+                if (modulo != 0)
                 {
                     nb++;
                 }
             }
-           // Console.WriteLine("nb" + nb);
+            // Console.WriteLine("nb" + nb);
             //Console.WriteLine("modulo:" +modulo);
 
-            for(int i=0; i<(nb*500) ; i= i+500)
+            for (int i = 0; i < (nb * 500); i = i + 500)
             {
-                if(nb ==1)
+                if (nb == 1)
                 {
                     Anonymize(chemin, nom, 0, nbfilename, filename);
                 }
 
                 else
                 {
-                    if( (nb*500) > (i+500))
+                    if ((nb * 500) > (i + 500))
                     {
-                        Anonymize(chemin, nom, i, i + 500,filename);
+                        Anonymize(chemin, nom, i, i + 500, filename);
                     }
                     else
                     {
-                        Anonymize(chemin, nom, i, nbfilename,filename);
+                        Anonymize(chemin, nom, i, nbfilename, filename);
                     }
                 }
             }
 
             return val;
-         }
+        }
 
     }
 }
